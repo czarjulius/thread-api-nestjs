c7e7de2db996b1c0c74aef24900cf7b7e1f8d702
czarjulius <juliusczar.jc@gmail.com>
2023-10-10 09:35:36 +0100

    complete backend

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Query,
+  BadRequestException,
+} from '@nestjs/common';
 import { CommentsService } from './comments.service';
 import { CreateCommentDto } from './dto/create-comment.dto';
 // import { UpdateCommentDto } from './dto/update-comment.dto';
@@ -13,8 +20,18 @@ export class CommentsController {
   }
 
   @Get()
-  findAll() {
-    return this.commentsService.findAll();
+  findAll(@Query() queryParams) {
+    if (queryParams.parentId) {
+      try {
+        return this.commentsService.getCommentsByParentId(queryParams.parentId);
+      } catch (error) {
+        throw new BadRequestException('Something bad happened', {
+          cause: new Error(error.message),
+          description: 'Some error description',
+        });
+      }
+    }
+    return this.commentsService.getTopLevelComments();
   }
 
   // @Get(':id')
--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -26,6 +26,18 @@ export class CommentsService {
   findAll() {
     return this.commentModel.find().populate(['user', 'parent']).exec();
   }
+  getTopLevelComments() {
+    return this.commentModel
+      .find({ parent: null })
+      .populate(['user', 'parent'])
+      .exec();
+  }
+  getCommentsByParentId(parentId: string) {
+    return this.commentModel
+      .find({ parent: parentId })
+      .populate(['user', 'parent'])
+      .exec();
+  }
 
   // findOne(id: number) {
   //   return `This action returns a #${id} comment`;
